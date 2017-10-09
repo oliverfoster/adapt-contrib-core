9b9ece3bfd9439630206dfdcfbbd181f0e821132
Oliver Foster <oliver.foster@kineo.com>
2017-10-09 21:47:51 +0100

    Removed adapt-contrib-boot

--- config.js
+++ config.js
@@ -1,20 +1,4 @@
 require.config({
-    paths: {
-        jquery: 'empty:',
-        underscore: 'empty:',
-        backbone: 'empty:',
-        modernizr: 'empty:',
-        handlebars: 'empty:',
-        velocity: 'empty:',
-        imageReady: 'empty:',
-        inview: 'empty:',
-        a11y: 'empty:',
-        scrollTo: 'empty:',
-        libraries: 'empty:',
-        bowser: 'empty:',
-        'core/js/libraries/bowser': 'empty:',
-        'coreJS/libraries/bowser': 'empty:'
-    },
     map: {
         '*': {
             components: '',
